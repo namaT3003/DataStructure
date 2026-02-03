75abd9673b0869a1f9e7091fd99926c763cc5935
namaT3003 <n.tiwari0138@gmail.com>
2026-02-03 13:50:17 +0530

    prime number or not

---
+++ Lecture14.java/primenumber.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class primenumber {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a number:");
+        int n = scanner.nextInt();
+        boolean ans=false;
+        for(int i=2;i<=n-1;i++){
+            if(n%i==0){
+                ans=true;
+                break;
+
+            }
+        }
+        if (ans){
+            System.out.println("it is not prime");
+        }
+        else{
+            System.out.println("it is prime");
+        }
+
+
+        scanner.close();
+    }
+}
+//done
