f68711292dd59aba7c57debab7dfed73c9f1464f
namaT3003 <n.tiwari0138@gmail.com>
2026-02-06 22:33:27 +0530

    Inverted right angled triangle

---
+++ SelfPractice/Patterns/InvertedTri.java
@@ -0,0 +1,14 @@
+package SelfPractice.Patterns;
+
+public class InvertedTri {
+    public static void main(String[] args) {
+        int n = 4;
+
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
