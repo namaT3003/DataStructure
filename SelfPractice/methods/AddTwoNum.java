fb9719dd2ad96f7f54f5899dddbc4520c131375a
namaT3003 <n.tiwari0138@gmail.com>
2026-02-08 22:43:58 +0530

    Add two numbers

---
+++ SelfPractice/methods/AddTwoNum.java
@@ -0,0 +1,12 @@
+//import java.util.Scanner;
+
+public class AddTwoNum {
+    static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        int sum = add(5, 3);
+        System.out.println(sum);
+    }
+}
