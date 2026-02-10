6b66b74a5079db05c4cf28be3eba9c7d39cf895b
namaT3003 <n.tiwari0138@gmail.com>
2026-02-10 22:33:37 +0530

    Count positive,negative and zero elements in an array

---
+++ SelfPractice/Arrays/CountElements.java
@@ -0,0 +1,26 @@
+package SelfPractice.Arrays;
+
+public class CountElements {
+    public static void main(String[] args) {
+
+        int[] arr = {1, -2, 0, 5, -3, 0};
+
+        int positive = 0;
+        int negative = 0;
+        int zero = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > 0) {
+                positive++;
+            } else if (arr[i] < 0) {
+                negative++;
+            } else {
+                zero++;
+            }
+        }
+
+        System.out.println("Positive numbers = " + positive);
+        System.out.println("Negative numbers = " + negative);
+        System.out.println("Zeros = " + zero);
+    }
+}
