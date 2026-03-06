0ce035a2ebf332d651f405f4bea6ae9fa252c96d
namaT3003 <n.tiwari0138@gmail.com>
2026-03-06 23:04:05 +0530

    Arraylist numbers

---
+++ SelfPractice/Arraylists/Numbers.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+public class Numbers {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        list.add(5);
+        list.add(15);
+        list.add(25);
+
+        System.out.println(list.size());
+    }
+}
