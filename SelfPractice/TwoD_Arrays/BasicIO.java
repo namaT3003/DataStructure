479d0b6efda21d7f4908137d77414c4b970292be
namaT3003 <n.tiwari0138@gmail.com>
2026-03-25 00:05:23 +0530

    2d array basic input and output

---
+++ SelfPractice/TwoD_Arrays/BasicIO.java
@@ -0,0 +1,28 @@
+package SelfPractice.TwoD_Arrays;
+import java.util.Scanner;
+public class BasicIO {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int r=scanner.nextInt();
+        int c=scanner.nextInt();
+        int[][]arr= new int[r][c];
+
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                arr[i][j]=scanner.nextInt();
+            }
+        }
+
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                System.out.print(arr[i][j]+" ");
+            }
+             System.out.println();
+        }
+       
+
+
+        scanner.close();
+    }
+}
