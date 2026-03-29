4703e92bca686aa7abce0c75e1d1729cad6c801e
namaT3003 <n.tiwari0138@gmail.com>
2026-03-29 23:15:41 +0530

    Array spiral

---
+++ SelfPractice/TwoD_Arrays/ArraySpiral.java
@@ -0,0 +1,50 @@
+package SelfPractice.TwoD_Arrays;
+
+import java.util.*;
+
+public class ArraySpiral {
+
+    public static void spiralPrint(int[][] arr) {
+        int top = 0;
+        int bottom = arr.length - 1;
+        int left = 0;
+        int right = arr[0].length - 1;
+
+        while (top <= bottom && left <= right) {
+
+            for (int i = left; i <= right; i++) {
+                System.out.print(arr[top][i] + " ");
+            }
+            top++;
+
+            for (int i = top; i <= bottom; i++) {
+                System.out.print(arr[i][right] + " ");
+            }
+            right--;
+
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--) {
+                    System.out.print(arr[bottom][i] + " ");
+                }
+                bottom--;
+            }
+
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    System.out.print(arr[i][left] + " ");
+                }
+                left++;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] arr = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        spiralPrint(arr);
+    }
+}
