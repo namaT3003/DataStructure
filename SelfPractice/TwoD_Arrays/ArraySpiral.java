06d185bea9544a4bb7736fa8dc23441a450ac2f6
namaT3003 <n.tiwari0138@gmail.com>
2026-03-30 23:31:01 +0530

    L-136 Single number

--- SelfPractice/TwoD_Arrays/ArraySpiral.java
+++ SelfPractice/TwoD_Arrays/ArraySpiral.java
@@ -39,7 +39,8 @@ public class ArraySpiral {
     }
 
     public static void main(String[] args) {
-        int[][] arr = {
+        int[][] arr = {'
+        /'
             {1, 2, 3},
             {4, 5, 6},
             {7, 8, 9}
