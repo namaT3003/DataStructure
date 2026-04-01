d7643cb8e50d7ae5e9eb1caf080b3d6aad5ef92f
namaT3003 <n.tiwari0138@gmail.com>
2026-04-01 23:31:11 +0530

    L-9 palindrome number

---
+++ SelfPractice/Leetcode/PalindromeNumber.java
@@ -0,0 +1,24 @@
+package SelfPractice.Leetcode;
+
+public class PalindromeNumber {
+
+    public static void main(String[] args) {
+        int x = 121;
+        System.out.println(isPalindrome(x));
+    }
+
+    public static boolean isPalindrome(int x) {
+        if (x < 0) return false;
+
+        int original = x;
+        int reversed = 0;
+
+        while (x != 0) {
+            int digit = x % 10;
+            reversed = reversed * 10 + digit;
+            x /= 10;
+        }
+
+        return original == reversed;
+    }
+}
