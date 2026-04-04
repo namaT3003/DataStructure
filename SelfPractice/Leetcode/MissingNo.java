10786ca3c47f8e79c89e195ba06a9289d09510ea
namaT3003 <n.tiwari0138@gmail.com>
2026-04-04 22:17:41 +0530

    L-268 missing number

---
+++ SelfPractice/Leetcode/MissingNo.java
@@ -0,0 +1,19 @@
+package SelfPractice.Leetcode;
+
+public class MissingNo {
+
+    public static void main(String[] args) {
+        int[] nums = {3, 0, 1};
+        System.out.println(missingNumber(nums));
+    }
+
+    public static int missingNumber(int[] nums) {
+        int xor = nums.length;
+
+        for (int i = 0; i < nums.length; i++) {
+            xor ^= i ^ nums[i];
+        }
+
+        return xor;
+    }
+}
